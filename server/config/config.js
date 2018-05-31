6ac7de0a0ff88042349e63d0af8d6ad2e946a0c3
pantacha <pantacha@hotmail.com>
2018-05-31 12:38:14 +0200

    comprobaciones

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,14 @@
-port = process.env.PORT || 3000;
+//Puertos
+process.env.PORT = process.env.PORT || 3000;
+
+//Entornos
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//BD
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb://cafe-user:123456@ds227740.mlab.com:27740/cafe';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -13,12 +13,14 @@ app.use(bodyParser.json());
 app.use(require('./routes/usuario'));
 
 
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
-    if (err) throw new err;
-    console.log('Base de datos ONLINE');
-
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) {
+        console.log('Error');
+    } else {
+        console.log('Base de datos ONLINE');
+    }
 });
 
-app.listen(port, () => { console.log(`Server escuchando en el puerto ${port}`) });
+app.listen(process.env.PORT, () => { console.log(`Server escuchando en el puerto ${process.env.PORT}`) });
 
 console.log(process);
